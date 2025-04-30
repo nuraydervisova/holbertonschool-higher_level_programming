a549847faa728ce7241ac2d60b9cb4be7ae06a03
nuraydervisova <nuraydervisova@gmail.com>
2025-04-30 14:48:27 +0000

    mytask2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 for i in range(97, 123):  # ASCII dəyərləri 97-122 (a-dan z-yə)
-    print("{}".format(chr(i)), end="")  # `format()` istifadə edərək çap ed
+    print("{}".format(chr(i)), end="")  # `format()` istifadə edərək çap edirik
