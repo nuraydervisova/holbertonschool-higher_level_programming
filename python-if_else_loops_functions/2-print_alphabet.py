6f443fe9d25b0f01a3087341c9725d7745c0908a
nuraydervisova <nuraydervisova@gmail.com>
2025-04-30 14:46:40 +0000

    mytask2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 for i in range(97, 123):  # ASCII dəyərləri 97-122 (a-dan z-yə)
-    if i <= 105:  # 79 simvola bölmək üçün iki sətirə ayırırıq
-        print("{}".format(chr(i)), end="")
-    else:
-        print("{}".format(chr(i)), end="")
+    print("{}".format(chr(i)), end="")  # `format()` istifadə edərək çap ed
