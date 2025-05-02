b33ea25a68452eab1f289b8f2c3f7d67b9dbf62d
nuraydervisova <nuraydervisova@gmail.com>
2025-05-02 09:08:49 +0000

    my task0

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a, b):
+    return a + b
+    from add_0 import add
+    if __name__ == "__main__":
+        a = 1
+        b = 2
+        result = add(a, b)
+        print(f"{a} + {b} = {result}")
+~                                           
