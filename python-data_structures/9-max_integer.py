60e7477ebc09a51c39539c019da5f116ef3d5b2c
nuraydervisova <nuraydervisova@gmail.com>
2025-05-02 12:32:04 +0000

    mytask46

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:  # Siyahı boşdur
+    if not my_list:  # Eğer liste boş
         return None
-    max_num = my_list[0]  # İlk elementi ən böyük olaraq qəb
-    for num in my_list:  # Siyahı üzərində hər bir elementi 
+    max_num = my_list[0]  # Başlangıçta listenin ilk elemanını en büyük olarak kabu
+    for num in my_list:  # Listenin her elemanını kontrol 
         if num > max_num:
-            max_num = num  # Əgər daha böyük bir element tapsaq, onu ən böyük kimi t�
-    return max_num
+            max_num = num  # Daha büyük bir sayı bulunduysa, onu yeni en büyük sayı olar
+    return max_num  # En büyük sayıyı d�
