57381c1e639e01911da9e714eb0c5af061dc01d9
nuraydervisova <nuraydervisova@gmail.com>
2025-05-02 11:40:05 +0000

    mytask23

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        return my_list.copy()  # idx mənfi və ya siyahıdan kənarda olduqda, orijinal siyahının kopyasını qaytarır
-    new_list = my_list.copy()  # Orijinal siyahını kopyalayır
-    new_list[idx] = element  # Kopyalanmış siyahıda elementi dəyişdirir
-    return new_list  # Dəyişdirilmiş siyahını qaytarır
+        return my_list.copy()  # idx mənfi və ya siyahıdan kənarda olduqda, orijinal siyahının kopyasını 
+    new_list = my_list.copy()  # Orijinal siyahını kopyalay
+    new_list[idx] = element  # Kopyalanmış siyahıda elementi dəyiş
+    return new_list  # Dəyişdirilmiş siyahını qay
