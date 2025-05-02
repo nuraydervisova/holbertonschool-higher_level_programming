366cebce26f324a91bb01bed67740ee0031abb52
nuraydervisova <nuraydervisova@gmail.com>
2025-05-02 11:42:36 +0000

    mytask23

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    """Replaces an element in a list at a specific position
+    without modifying the original list"""
     if idx < 0 or idx >= len(my_list):
-        return my_list.copy()  # idx mənfi və ya siyahıdan kənarda olduqda, orijinal siyahının kopyasını 
-    new_list = my_list.copy()  # Orijinal siyahını kopyalay
-    new_list[idx] = element  # Kopyalanmış siyahıda elementi dəyiş
-    return new_list  # Dəyişdirilmiş siyahını qay
+        # Check if the index is out of range or negative
+        return my_list.copy()  # Return a copy of the original list
+    new_list = my_list.copy()  # Create a copy of the original list
+    new_list[idx] = element  # Replace the element at the specific index
+    return new_list  # Return the modified copy
