faa5e6dfd98bd73ff957bb7edaee66028ef8f903
nurardervisova <nuraydervisova@gmail.com>
2025-06-10 12:38:22 +0000

    myatsk7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,9 +7,15 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
+
+# Yoxla: fayl mövcuddursa, mövcud siyahını oxu, əks halda boş siyahı ilə başla
 if exists(filename):
     items = load_from_json_file(filename)
 else:
     items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, filename)
+
+# Əlavə olunan arqumentləri siyahıya əlavə et
+items.extend(sys.argv[1:])
+
+# Yenilənmiş siyahını fayla yaz
+save_to_json_file(items, filename)
