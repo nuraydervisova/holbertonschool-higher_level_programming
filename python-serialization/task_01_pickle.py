ab8b48f7536cd6e1bc31f7b6e7f44215ffa52704
nuraydervisova <nuraydervisova.com>
2025-06-11 18:32:12 +0400

    mytask01

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import pickle
 
 class CustomObject:
@@ -13,37 +12,22 @@ class CustomObject:
         print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
-        """Obyektin vəziyyətini fayla yazır."""
         try:
-            with open(filename, 'wb') as f:
-                pickle.dump(self, f)
-            print(f"Obyekt '{filename}' faylına yazıldı.")
+            with open(filename, 'wb') as file:
+                pickle.dump(self, file)
         except Exception as e:
-            print(f"Xəta baş verdi: {e}")
+            print(f"Serialization failed: {e}")
 
     @classmethod
     def deserialize(cls, filename):
-        """Fayldan obyektin vəziyyətini oxuyur və obyekt yaradır."""
         try:
-            with open(filename, 'rb') as f:
-                obj = pickle.load(f)
-            print(f"Obyekt '{filename}' faylından oxundu.")
-            return obj
-        except (FileNotFoundError, pickle.UnpicklingError) as e:
-            print(f"Xəta baş verdi: {e}")
+            with open(filename, 'rb') as file:
+                obj = pickle.load(file)
+                if isinstance(obj, cls):
+                    return obj
+                else:
+                    print("Deserialized object is not of type CustomObject.")
+                    return None
+        except (FileNotFoundError, pickle.PickleError, EOFError, Exception) as e:
+            print(f"Deserialization failed: {e}")
             return None
-
-if __name__ == "__main__":
-    # Orijinal obyektin yaradılması
-    obj = CustomObject(name="John", age=25, is_student=True)
-    print("Orijinal Obyekt:")
-    obj.display()
-
-    # Obyekti seriyalaşdırmaq
-    obj.serialize("object.pkl")
-
-    # Obyekti deserializasiya etmək
-    new_obj = CustomObject.deserialize("object.pkl")
-    if new_obj:
-        print("\nDeserializasiya olunmuş Obyekt:")
-        new_obj.display()
